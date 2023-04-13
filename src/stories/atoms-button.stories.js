b3da36dab61532bf4c6aa2fdd46826f2bfefc21a
fukuyuki918 <y.fukumitsu.23.3.814@4nonome.com>
2023-04-13 14:45:12 +0900

    stories追加

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import Item_Button from "../components/atoms/example/example.html";
+import "../components/atoms/example/example.scss";
+
+export default { title: "Atoms/Button" };
+
+export const Button = () => Item_Button;
