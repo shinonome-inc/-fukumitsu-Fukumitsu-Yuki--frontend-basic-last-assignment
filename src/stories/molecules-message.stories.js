97be92585e61217349405a1331e5e0ca09d93e0d
fukuyuki918 <y.fukumitsu.23.3.814@4nonome.com>
2023-04-17 13:47:36 +0900

    html scss変更

--- src/stories/molecules-message.stories.js
+++
@@ -1,7 +0,0 @@
-import Item_Message from "../components/molecules/message/message.html";
-import "../components/molecules/message/message.scss";
-import { Default } from "./atoms-button.stories";
-
-export default { title: "Molecules/Message" };
-
-Default = () => Item_Message;
